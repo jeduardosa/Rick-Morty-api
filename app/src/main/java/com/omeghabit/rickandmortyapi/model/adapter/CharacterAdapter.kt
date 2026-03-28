57970ff435a3a4ff1802c8b33f9eb20f02cddb89
omegha <jeduardosa86@gmail.com>
2026-03-28 12:05:50 -0300

    Pequena mudança na exibição do Alive ou dead.

--- app/src/main/java/com/omeghabit/rickandmortyapi/model/adapter/CharacterAdapter.kt
+++ app/src/main/java/com/omeghabit/rickandmortyapi/model/adapter/CharacterAdapter.kt
@@ -1,6 +1,7 @@
 package com.omeghabit.rickandmortyapi.model.adapter
 
 import android.annotation.SuppressLint
+import android.util.Pair
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
@@ -9,6 +10,7 @@ import coil.load
 import com.omeghabit.rickandmortyapi.R
 import com.omeghabit.rickandmortyapi.databinding.ItemCharacterBinding
 import com.omeghabit.rickandmortyapi.databinding.ItemCharacterBinding.inflate
+import java.util.Collections.emptyList
 
 class CharacterAdapter( private var characters: List<Character> = emptyList()
 ) : RecyclerView.Adapter<CharacterAdapter.CharHolder>() {
@@ -37,10 +39,11 @@ class CharacterAdapter( private var characters: List<Character> = emptyList()
                 placeholder(R.drawable.ic_launcher_background)
 
                 val (colorRes, bgRes) = when (character.status.lowercase()) {
-                    "alive" -> Pair(R.color.green, R.drawable.status_alive_bg)
-                    "dead" -> Pair(R.color.red, R.drawable.status_dead_bg)
-                    else -> Pair(R.color.gray, R.drawable.status_unknown_bg)
+                    "alive" -> R.color.green to R.drawable.status_alive_bg
+                    "dead" -> R.color.red to R.drawable.status_dead_bg
+                    else -> R.color.gray to R.drawable.status_unknown_bg
                 }
+
                 binding.tvStatus.setTextColor(ContextCompat.getColor(binding.root.context, colorRes))
                 binding.tvStatus.background = (ContextCompat.getDrawable(binding.root.context, bgRes))
 
